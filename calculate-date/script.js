53e2cdb88fe4da9e57768772956d85ad8d44e8c1
chienbm <chien_buiminh@vn.msig-asia.com>
2025-09-16 15:59:31 +0700

    [master] feat: clear old test git + add tool calculate date

---
+++ calculate-date/script.js
@@ -0,0 +1,141 @@
+$(document).ready(function () {
+  $('#sDate, #eDate').datetimepicker({
+    format: 'DD/MM/YYYY',
+  });
+
+  $('input[name=optRadio]').change(function (e) {
+    $('div.type').hide();
+    $('div.type input[type=text]').val('');
+    $('div.type.type-' + $(this).val()).show();
+  });
+
+  $('#btnAction').on('click', function (e) {
+    if ($('#sDate').val() == '') {
+      alert('Vui lòng nhập ngày bắt đầu!!!');
+      return;
+    }
+    let sDate = moment($('#sDate').val(), 'DD/MM/YYYY');
+
+    const lstDate = $('form').serializeArray();
+    const lstChoose = [];
+    const arrResult = {
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+    };
+    let countDate = 0;
+    let maxRow = 0;
+
+    if (lstDate.length > 0) {
+      $.each(lstDate, function (i, e) {
+        if (e.name == 'date[]') {
+          lstChoose.push(+e.value);
+        }
+      });
+    }
+
+    if ($('input[name=optRadio]:checked').val() == 1) {
+      if ($('#eDate').val() == '') {
+        alert('Vui lòng nhập ngày kết thúc!!!');
+        return;
+      }
+
+      let eDate = moment($('#eDate').val(), 'DD/MM/YYYY');
+
+      if (moment(eDate).isBefore(sDate)) {
+        alert('Ngày kết thúc phải lớn hơn ngày bắt đầu!!!');
+        return;
+      }
+
+      while (moment(sDate).isSameOrBefore(eDate)) {
+        if (lstChoose.length > 0) {
+          if (!lstChoose.includes(moment(sDate).isoWeekday())) {
+            arrResult[moment(sDate).isoWeekday()] = [
+              ...arrResult[moment(sDate).isoWeekday()],
+              moment(sDate).format('DD/MM/YYYY'),
+            ];
+            countDate++;
+          }
+        } else {
+          arrResult[moment(sDate).isoWeekday()] = [
+            ...arrResult[moment(sDate).isoWeekday()],
+            moment(sDate).format('DD/MM/YYYY'),
+          ];
+          countDate++;
+        }
+        sDate = new Date(moment(sDate, 'YYYYMMDD', true).add('days', 1).format());
+      }
+    } else {
+      if ($('#totalDate').val() == '') {
+        alert('Vui lòng nhập tổng số ngày!!!');
+        return;
+      }
+
+      if (lstChoose.length == 7) {
+        return;
+      }
+
+      let tempTotal = 0;
+      const typeCalc = $('input[name=optCalc]:checked').val();
+      while (tempTotal < +$('#totalDate').val()) {
+        if (lstChoose.length > 0) {
+          if (!lstChoose.includes(moment(sDate).isoWeekday())) {
+            arrResult[moment(sDate).isoWeekday()] = [
+              ...arrResult[moment(sDate).isoWeekday()],
+              moment(sDate).format('DD/MM/YYYY'),
+            ];
+            countDate++;
+            if (typeCalc == 1) {
+              tempTotal++;
+            }
+          }
+        } else {
+          arrResult[moment(sDate).isoWeekday()] = [
+            ...arrResult[moment(sDate).isoWeekday()],
+            moment(sDate).format('DD/MM/YYYY'),
+          ];
+          countDate++;
+          if (typeCalc == 1) {
+            tempTotal++;
+          }
+        }
+        sDate = new Date(moment(sDate, 'YYYYMMDD', true).add('days', 1).format());
+        if (typeCalc == 2) {
+          tempTotal++;
+        }
+      }
+    }
+
+    let lastRowEachCol = '<tr>';
+    let lastRowTotal = `<tr><td colspan="7">Tổng số ngày: <code>${countDate}</code></td></tr>`;
+    $.each(arrResult, function (i, e) {
+      const length = e.length;
+      if (length > 0 && length > maxRow) {
+        maxRow = length;
+      }
+      lastRowEachCol += `<td>${length}</td>`;
+    });
+    lastRowEachCol += '</tr>';
+    if (maxRow > 0) {
+      let html = '';
+      for (let i = 0; i < maxRow; i++) {
+        html += `<tr>`;
+        $.each(arrResult, function (j, e) {
+          if (typeof arrResult[j][i] !== 'undefined') {
+            html += `<td>${arrResult[j][i]}</td>`;
+          } else {
+            html += '<td></td>';
+          }
+        });
+        html += `</tr>`;
+      }
+      html += lastRowEachCol;
+      html += lastRowTotal;
+      $('#tblResult tbody').html(html);
+    }
+  });
+});
